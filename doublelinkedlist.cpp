e3771426562083125be91b32d4714e8e0e82679c
Deva658 <devaaditya271@gmail.com>
2025-05-15 11:12:31 +0700

    Mulai implementasi fungsi untuk menelusuri list dari belakang

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -149,5 +149,21 @@ public:
         }
     }
 
-   
+    void revtraverse()
+    {
+        if (START == NULL)
+        {
+            cout << "\nList is empty" << endl;
+            return;
+        }
+
+        //step 1: move to last node
+        Node *currentNode = START;
+        int i = 0;
+        while (currentNode->next != NULL)
+        {
+            currentNode = currentNode->next;
+            i++;
+        }
+
 };
