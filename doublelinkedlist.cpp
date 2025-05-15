c069a06581553acd80c0ceb2453cf751cb12395d
Deva658 <devaaditya271@gmail.com>
2025-05-15 09:44:23 +0700

    membuat library

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -2,17 +2,4 @@
 #include <string>
 using namespace std;
 
-class Node
-{
-public:
-    int noMhs;
-    Node *next;
-    Node *prev;
-};
 
-class DoubleLinkedList
-{
-private:
-    Node *START;
-    
-}
