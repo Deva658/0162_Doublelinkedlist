8ff06a882b07fb33150cbb5ddaed1d0c5578a8ea
Deva658 <devaaditya271@gmail.com>
2025-05-15 10:24:44 +0700

    Menambahkan logika untuk menyisipkan node di awal list

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -28,6 +28,58 @@ public:
         cout << "\nEnter the roll number of the student: ";
         cin >> nim;
 
+                // step 1: aloicate memory for new node
+        Node *newNode = new Node();
 
+        // step 2: assign value to the data field 
+        newNode->noMhs = nim;
+
+        // step 3: insert at begibning if list is empty or nim is smallest
+        if (START == NULL || nim <= START-> noMhs)
+        {
+            if (START != NULL && nim == START->noMhs)
+            {
+                cout << "\nDuplicate number not allowed" << endl;
+                return;
+            }
+            // step 4: newNode.next = START
+            newNode->next = START;
+
+            // step 5: START.prev = newNode (if START exists)
+            if (START != NULL)
+                START->prev = newNode;
+
+            // step 6: newNode.prev = NULL
+            newNode->prev = NULL;
+
+            // step 7: START = newNode
+            START = newNode;
+            return;
+        }
+
+        // iusert in between node
+        // step 8: locate position for insertion
+        Node *current = START;
+        while (current->next != NULL && current->next->noMhs < nim)
+        {
+            current = current->next;
+        }
+
+        if (current->next != NULL && nim == current->next->noMhs)
+        {
+            cout << "\nDuplicate roll numbers not allowed" << encl;
+            return;
+        }
+
+        // step 9: insert bertween current and currentr->next
+        newNode->next = current->next;
+        newNode->prev = current;
+
+        //insert last node
+        if (current->next != NULL)
+            current->next->prev = newNode;
+
+        current->next = newNode;
     }
+
 }
